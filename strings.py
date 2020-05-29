9dffe8d24053961eb055e57771c607e6f831426a
a7nb88 <46125928+a7nb88@users.noreply.github.com>
2020-05-29 22:12:39 +0400

    first commit

---
+++ strings.py
@@ -0,0 +1,13 @@
+# Example_1
+name = 'anb'
+print(name)
+print(id(name))
+print(hex(id(name)))
+# Example_2
+fname ='an'
+lname = 'b'
+print(fname + " " + lname)
+print(fname.upper() + " " + lname.upper())
+# Example_3
+full_name = 'anb fad'
+print(full_name.replace('fad', 'rbd'))
