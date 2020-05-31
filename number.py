2277182daf1d451a547d33f03123a61315d687b8
a7nb88 <albattashi988@gmail.com>
2020-05-31 04:27:02 +0400

    added number code

---
+++ number.py
@@ -0,0 +1,17 @@
+print(2 + 3)
+print(int('3') * 4)
+print(3 * 4)
+print(5 - 2)
+print(3 ** 10)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+
+num1 = 20.5
+num2 = 13
+num2 -=1
+num3 = num1 + num2
+num3 = num3 + 1
+num3 += 1
+print(num3)
+print(type(num3))
