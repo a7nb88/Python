ff150ef58df0b3e3db6a4f6b270d7f75b7bdcc7a
a7nb88 <46125928+a7nb88@users.noreply.github.com>
2020-06-01 23:20:18 +0400

    first commit

---
+++ operators.py
@@ -0,0 +1,32 @@
+num1 = 5
+num2 = 2
+print(num1 == num2)
+print(num1 != num2)
+print(num1 < num2)
+print(num1 > num2)
+print(num1 <= num2)
+print(num1 >= num2)
+print(num1 <= num2)
+
+print(5 != 6)
+print(6 > 4)
+print(6 == 5)
+print(6 >= 5)
+
+print(num1 == num2 and num1 != num2)
+print(num1 < num2 and num1 == num2)
+print(num1 < num2 and num1 != num2)
+print(num1 > num2 and num1 != num2)
+
+print(num1 == num2 or num1 != num2)
+print(num1 < num2 or num1 == num2)
+print(num1 < num2 or num1 != num2)
+print(num1 > num2 or num1 != num2)
+
+print(not (num1 == num2))
+print(not (num1 != num2))
+print(not (num1 >= num2))
+print(not (num1 <= num2))
+
+print(not (num1 == num2 and num1 >=num2))
+print(not (num1 == num2 or num1 >=num2))
