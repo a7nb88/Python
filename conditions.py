063d470febbdc1b58727cf50327dfa7655f23df8
a7nb88 <46125928+a7nb88@users.noreply.github.com>
2020-06-03 21:20:06 +0400

    first commit

---
+++ conditions.py
@@ -0,0 +1,6 @@
+username = input('Please enter your username: ')
+password = input('Please enter your password: ')
+if username == 'anb' and password == '123':
+    print('welcome to your email')
+else:
+    print('Error')
