8d8fb0e09b6753edd76c0805cf2e4d0acff37e62
a7nb88 <46125928+a7nb88@users.noreply.github.com>
2020-08-03 20:47:05 +0400

    first commit

---
+++ functions.py
@@ -0,0 +1,14 @@
+def linear():
+    print("-----------")
+
+def addition(n1,n2):
+    print(int(n1) + int(n2))
+
+n1 = input("Please enter first number: ")
+n2 = input("Please enter second number: ")
+linear()
+addition(n1,n2)
+linear()
+print("Fadi Alaswadi")
+linear()
+
