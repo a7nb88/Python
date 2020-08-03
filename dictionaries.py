8853edaad6b7c3ba8f51b7dfd54ca5179d2d1ed7
a7nb88 <46125928+a7nb88@users.noreply.github.com>
2020-08-03 20:30:33 +0400

    first commit

---
+++ dictionaries.py
@@ -0,0 +1,6 @@
+students = {"Arabic": 3 , "English": 2 , "Math": 4}
+print(students)
+students["Arabic"] = ["Ahmad" , "Hosny" , "Khaled"]
+print(students)
+students.update({"History": ["Amjad" , "Fadi"]})
+print(students)
